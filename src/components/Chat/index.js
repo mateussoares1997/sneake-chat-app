7a09fe531a92f83b68a084e6d32e0074ae78a60a
Mateus Soares <mateus.silva@pravaler.com.br>
2020-05-20 13:18:04 -0300

    Sending and receiving messages

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,30 +1,67 @@
-import React, { useEffect } from 'react';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+let socket
 
 const Chat = (props) => {
 
     const { userName, setUserName } = props
+    const [ messages, setMessages ] = useState([])
+    const [ text, setText ] = useState('')
 
     useEffect(() => {
-        const socket = io(process.env.REACT_APP_CHAT_SERVER)
+
+        socket = io(process.env.REACT_APP_CHAT_SERVER)
 
         socket.on('connect', () => {
             socket.emit('join', {
-                room: 'room'
+                room: 'room',
+                who: userName
             });
         });
 
         socket.on('joined',(data) => {
-            alert('joined')
+
+            if(!data.includes(userName)){
+                alert(data)
+            }else{
+                alert('You entered')
+            }
+
+        });
+       
+        socket.on('messaged',(data) => {
+
+            if(data.who !== userName){
+                alert(data.text)
+            }
+
         });
 
     }, [userName])
 
+    const handleChange = (event) => {
+        const value  = event.target.value
+        setText(value)
+    }
     
+    const handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            socket.emit('message', {
+                who: userName,
+                text: text,
+                room: 'room'
+            });
+            setText('')
+        }
+    }
     
     return (
         <div className="chat">
-            Let's chat, {userName}!
+            <ul className="chat-list"> 
+                
+            </ul>
+            <textarea onChange={handleChange} className="chat-field" value={text} onKeyPress={handleKeyPress}></textarea>
         </div>
     );
 }
